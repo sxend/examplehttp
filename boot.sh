b902839ddecd1b597c391314d2a5b2bc14fb69dd
sxend <arimitsu.k+github@gmail.com>
2019-01-23 11:08:28 +0900

    fix port

--- boot.sh
+++ boot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 PORT=$1
-PORT=${PORT:-80}
+PORT=${PORT:-9000}
 
 git pull origin master
 
- cargo run --release --example main -- --port=$PORT
+cargo run --release --example main -- --port=${PORT}
