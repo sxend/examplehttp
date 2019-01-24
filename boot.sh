cf60e79eb0d08b4e1cd00b8fce5006e2c757e1de
sxend <arimitsu.k+github@gmail.com>
2019-01-24 22:22:31 +0900

    fix boot

--- boot.sh
+++ boot.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-PORT=$1
-PORT=${PORT:-8888}
-USE_LOOP=$2
-USE_LOOP=${USE_LOOP:-true}
-
 git pull origin master
 
-RUST_BACKTRACE=1 cargo run --release --example main -- --port=${PORT} --use-loop=${USE_LOOP}
+RUST_BACKTRACE=1 RUST_LOG=main cargo run --release --example main -- $@
--- tools/gatling/src/test/scala/examplehttp/Bench.scala
+++ tools/gatling/src/test/scala/examplehttp/Bench.scala
@@ -1,14 +1,15 @@
 package examplehttp
 
+import com.typesafe.config.ConfigFactory
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
 
 class Bench extends Simulation {
-
+  val config = ConfigFactory.load
   val httpConf = http
-    .baseURL("http://0.0.0.0:8888")
+    .baseURL(s"http://${config.getString("target-address")}")
     .userAgentHeader("Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
 
   val scn = scenario("HealthSimulation")
